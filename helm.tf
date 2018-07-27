a2659b516f8cc6dcf401f06be838de113ff6569c
Scott Guymer <scott@scottguymer.co.uk>
2018-07-27 16:29:47 +0100

    POC of using helm from terraform to install deps

---
+++ helm.tf
@@ -0,0 +1,35 @@
+module "helm_aks1" {
+  source = "helm"
+
+  client_certificate     = "${base64decode(module.aks_cluster_1.cluster_client_certificate)}"
+  client_key             = "${base64decode(module.aks_cluster_1.cluster_client_key)}"
+  cluster_ca_certificate = "${base64decode(module.aks_cluster_1.cluster_ca)}"
+  host                   = "${module.aks_cluster_1.host}"
+}
+
+module "helm_aks2" {
+  source = "helm"
+
+  client_certificate     = "${base64decode(module.aks_cluster_2.cluster_client_certificate)}"
+  client_key             = "${base64decode(module.aks_cluster_2.cluster_client_key)}"
+  cluster_ca_certificate = "${base64decode(module.aks_cluster_2.cluster_ca)}"
+  host                   = "${module.aks_cluster_2.host}"
+}
+
+module "helm_gke1" {
+  source = "helm"
+
+  client_certificate     = "${base64decode(module.gke_cluster_1.cluster_client_certificate)}"
+  client_key             = "${base64decode(module.gke_cluster_1.cluster_client_key)}"
+  cluster_ca_certificate = "${base64decode(module.gke_cluster_1.cluster_ca)}"
+  host                   = "${module.gke_cluster_1.host}"
+}
+
+module "helm_gke2" {
+  source = "helm"
+
+  client_certificate     = "${base64decode(module.gke_cluster_2.cluster_client_certificate)}"
+  client_key             = "${base64decode(module.gke_cluster_2.cluster_client_key)}"
+  cluster_ca_certificate = "${base64decode(module.gke_cluster_2.cluster_ca)}"
+  host                   = "${module.gke_cluster_2.host}"
+}
