721626af2f40e5bb6b189b79f049e5d67395835d
lawrencegripper <info@grippers.co.uk>
2018-08-21 11:48:02 +0100

    Add note about dependson workaround

--- k8s/out.tf
+++ k8s/out.tf
@@ -1,6 +1,9 @@
 output "cluster_ingress_ip" {
   value = "${kubernetes_service.ingress_service.load_balancer_ingress.0.ip}"
 
+  //The helm elements depend on some of the items created in this module
+  // as modules don't support the depends_on syntax we abuse this output variable
+  // to have the same effect. 
   depends_on = [
     "kubernetes_deployment.hc-app",
     "kubernetes_ingress.hc-ingress",
