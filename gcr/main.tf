c17f3ab1bedbe5d533dcfc7d82dabaf5e9e158e4
Joe Swan <joseph.swan@sky.uk>
2019-03-06 17:17:28 +0000

    Sorting dependencies

--- gcr/main.tf
+++ gcr/main.tf
@@ -6,7 +6,7 @@ data "google_container_registry_repository" "registry" {
 }
 
 resource "google_service_account" "registry_user" {
-  depends_on   = ["project_apis"]
+  depends_on   = ["google_project_services.project_apis"]
   account_id   = "registryuser"
   display_name = "Registry User"
   project      = "${var.google_project_id}"
