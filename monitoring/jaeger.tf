349dff9ac88af8ac0e66c114d9e8f76d2f843b43
Paul Grave <paul.grave@sky.uk>
2019-03-08 12:11:18 +0000

    added: reference to incubator repo for jaeger

--- monitoring/jaeger.tf
+++ monitoring/jaeger.tf
@@ -6,10 +6,16 @@ data "template_file" "jaeger_values" {
   }
 }
 
+data "helm_repository" "incubator" {
+  name = "incubator"
+  url  = "https://kubernetes-charts-incubator.storage.googleapis.com/"
+}
+
 resource "helm_release" "jaeger" {
-  name      = "jaeger"
-  chart     = "incubator/jaeger"
-  namespace = "monitoring"
+  name       = "jaeger"
+  chart      = "jaeger"
+  namespace  = "monitoring"
+  repository = "{data.helm_repository.incubator.metadata.0.name}"
 
   # workaround to stop CI from complaining about keyring change
   keyring = ""
