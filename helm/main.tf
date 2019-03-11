971279281748100d3187be20b1709a3799ab3d79
Paul Grave <paul.grave@sky.uk>
2019-03-11 13:45:11 +0000

    fixed: (potentially) RBAC issue with registry-rewriter on AKS

--- helm/main.tf
+++ helm/main.tf
@@ -73,7 +73,7 @@ resource "helm_release" "prometheus" {
 
 resource "helm_release" "registry_rewriter" {
   name      = "registry-rewriter"
-  chart     = "https://github.com/lawrencegripper/MutatingAdmissionsController/releases/download/v0.1.1/registry-rewriter-0.1.0.tgz"
+  chart     = "https://github.com/paulgrav/MutatingAdmissionsController/releases/download/1.1.2-rc1/registry-rewriter-0.1.2.tgz"
   namespace = "kube-system"
 
   # workaround to stop CI from complaining about keyring change
