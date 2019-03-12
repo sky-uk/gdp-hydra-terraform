007eb8029751e53a472e5c24b26813a78192b7ac
Paul Grave <paul.grave@sky.uk>
2019-03-12 10:29:38 +0000

    added: a pause in the workflow to facilitate debugging updated: reg rewriter to rc6 fixed: missing api group in the cluster role binding for reg writer

--- k8s/k8s.tf
+++ k8s/k8s.tf
@@ -58,7 +58,7 @@ resource "kubernetes_cluster_role_binding" "registry_rewriter" {
   }
 
   role_ref {
-    api_group = ""
+    api_group = "rbac.authorization.k8s.io"
     kind      = "ClusterRole"
     name      = "cluster-admin"
   }
