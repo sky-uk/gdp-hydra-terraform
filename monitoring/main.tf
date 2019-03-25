d0e041fa11ce3621e44fca1c8ef08f90db20244f
Joe Swan <joseph.swan@sky.uk>
2019-03-25 17:26:22 +0000

    More removal

--- monitoring/main.tf
+++ monitoring/main.tf
@@ -1,6 +1,4 @@
 provider "kubernetes" {
-  client_certificate     = "${base64decode(module.monitoring_cluster.cluster_client_certificate)}"
-  client_key             = "${base64decode(module.monitoring_cluster.cluster_client_key)}"
   cluster_ca_certificate = "${base64decode(module.monitoring_cluster.cluster_ca)}"
   host                   = "${module.monitoring_cluster.host}"
 }
