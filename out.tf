03822c286c1b38138749987981eb279116e0c8ad
Joe Swan <joseph.swan@sky.uk>
2019-03-26 16:01:09 +0000

    updated variables

--- out.tf
+++ out.tf
@@ -55,20 +55,3 @@ output "monitoring_prometheus_username" {
 output "monitoring_prometheus_password" {
   value = "${random_string.prom_metrics_password.result}"
 }
-
-output "monitoring_cluster_ips" {
-  value = "${data.kubernetes_service.ingress.load_balancer_ingress.0.ip}"
-}
-
-output "fluentd_ingress_ip" {
-  value = "${data.kubernetes_service.fluentd.load_balancer_ingress.0.ip}"
-}
-
-output "tiller_service_account_name" {
-  value = "${kubernetes_service_account.tiller.metadata.0.name}"
-}
-
-output "kubeconfig" {
-  value     = "${module.monitoring_cluster.kubeconfig}"
-  sensitive = true
-}
