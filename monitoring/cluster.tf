ce44dc10908be1a35ffeb4272c31e40dcc455411
Joe Swan <joseph.swan@sky.uk>
2019-03-26 15:24:28 +0000

    Revert "moved the cluster creation to main hydra file"
    This reverts commit 061eac05b7895d6d4adb1453801f2b8281e35c3f.

--- monitoring/cluster.tf
+++ monitoring/cluster.tf
@@ -9,6 +9,20 @@ variable "node_sku" {}
 
 variable "azure_resource_location" {}
 
+module "monitoring_cluster" {
+  source = "../gke"
+
+  project_name = "${var.project_name}"
+  tags         = "${var.tags}"
+
+  cluster_prefix     = "${var.cluster_prefix}"
+  region             = "europe-west2-a"
+  google_project     = "${var.google_project}"
+  kubernetes_version = "${var.kubernetes_version}"
+  node_count         = "${var.node_count}"
+  machine_type       = "${var.machine_type}"
+}
+
 module "cluster_services_monitoring" {
   source = "../cluster_services"
 
