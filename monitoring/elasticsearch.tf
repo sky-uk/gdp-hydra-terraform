e71b45c02adbbac52fde78da4eee4583d5d95bc5
Joe Swan <joseph.swan@sky.uk>
2019-03-28 15:31:48 +0000

    moved provider for monitoring just in case

--- monitoring/elasticsearch.tf
+++ monitoring/elasticsearch.tf
@@ -8,19 +8,6 @@ resource "null_resource" "helm_init" {
   }
 }
 
-provider "helm" {
-  install_tiller  = true
-  service_account = "${var.tiller_service_account}"
-  tiller_image    = "gcr.io/kubernetes-helm/tiller:v2.11.0"
-
-  kubernetes {
-    host                   = "${var.host}"
-    cluster_ca_certificate = "${var.cluster_ca_certificate}"
-    username               = "${var.username}"
-    password               = "${var.password}"
-  }
-}
-
 data "template_file" "elasticsearch_values" {
   template = "${file("${path.module}/values/elasticsearch.values.yaml")}"
 
---
+++ monitoring/main.tf
@@ -0,0 +1,12 @@
+provider "helm" {
+  install_tiller  = true
+  service_account = "${var.tiller_service_account}"
+  tiller_image    = "gcr.io/kubernetes-helm/tiller:v2.11.0"
+
+  kubernetes {
+    host                   = "${var.host}"
+    cluster_ca_certificate = "${var.cluster_ca_certificate}"
+    username               = "${var.username}"
+    password               = "${var.password}"
+  }
+}
