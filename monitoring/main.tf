1f1bfc8a364e72062824e78c01bc146a7f4ede6a
Joe Swan <joseph.swan@sky.uk>
2019-03-28 16:09:08 +0000

    Added a kubnernetes provider

--- monitoring/main.tf
+++ monitoring/main.tf
@@ -1,3 +1,10 @@
+provider "kubernetes" {
+  host                   = "${var.host}"
+  cluster_ca_certificate = "${var.cluster_ca_certificate}"
+  username               = "${var.username}"
+  password               = "${var.password}"
+}
+
 provider "helm" {
   install_tiller  = true
   service_account = "${var.tiller_service_account}"
