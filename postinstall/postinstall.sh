2e56838b64f904899429b53286481ac5cd117be3
Paul Grave <paul.grave@sky.uk>
2019-03-29 13:49:43 +0000

    fixed: syntax error in the postinstall script

--- postinstall/postinstall.sh
+++ postinstall/postinstall.sh
@@ -33,7 +33,7 @@ function install_jaeger {
 function install_jaeger_monitoring {
     kubeconfig=$1
     kubectl create namespace observability
-    kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/crds/jaegertracing_v1_jaeger_crd.yaml (2)
+    kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/crds/jaegertracing_v1_jaeger_crd.yaml
     kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/service_account.yaml
     kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/role.yaml
     kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/role_binding.yaml
