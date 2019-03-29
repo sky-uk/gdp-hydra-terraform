fb4e5f8f7ef4f5fe4d622247698135758b5109ee
Paul Grave <paul.grave@sky.uk>
2019-03-29 14:39:18 +0000

    disabled: gke stackdriver

--- gke/main.tf
+++ gke/main.tf
@@ -21,6 +21,8 @@ resource "google_container_cluster" "cluster" {
     ]
   }
 
+  logging_service = "none"
+
   // Offset each cluster maintance windows by 2 hours from each other
   maintenance_policy {
     daily_maintenance_window {
