b79c0172c71cd6d74c09eb629cf432c8e7a4986e
Paul Grave <paul.grave@sky.uk>
2019-03-29 17:10:53 +0000

    fixed: kubeconfig not specified for the jaeger operator

--- postinstall/postinstall.sh
+++ postinstall/postinstall.sh
@@ -33,11 +33,11 @@ function install_jaeger {
 function install_jaeger_monitoring {
     kubeconfig=$1
     kubectl create namespace observability
-    kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/crds/jaegertracing_v1_jaeger_crd.yaml
-    kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/service_account.yaml
-    kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/role.yaml
-    kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/role_binding.yaml
-    kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/operator.yaml
+    kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/crds/jaegertracing_v1_jaeger_crd.yaml --kubeconfig=$kubeconfig
+    kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/service_account.yaml --kubeconfig=$kubeconfig
+    kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/role.yaml --kubeconfig=$kubeconfig
+    kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/role_binding.yaml --kubeconfig=$kubeconfig
+    kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/operator.yaml --kubeconfig=$kubeconfig
 
     kubectl create -f jaeger-monitoring/ --kubeconfig=$kubeconfig
 }
