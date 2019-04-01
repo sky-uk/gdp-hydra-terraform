424afbbe7fb75108a6609cf85b90e2c76e8c297f
Paul Grave <paul.grave@sky.uk>
2019-04-01 15:58:40 +0100

    moved: healthcheck back into terraform

--- postinstall/postinstall.sh
+++ postinstall/postinstall.sh
@@ -46,8 +46,8 @@ function install_jaeger_monitoring {
 get_configs
 for conf in $(find . -type f -name 'kubeconfig_*' | grep -v monitoring)
 do
-  echo "Installing hc-app for kubeconf: $conf"
-  install_hc_app $conf
+#   echo "Installing hc-app for kubeconf: $conf"
+#   install_hc_app $conf
   echo "Installing hotrod for kubeconf: $conf"
   install_hotrod $conf
   echo "Installing jaeger for kubeconf: $conf"
